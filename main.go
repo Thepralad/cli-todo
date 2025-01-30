ced6037399d44188d6b86c083aef035a7c05c7c9
thepralad <prgurung04@gmail.com>
2025-01-30 07:37:48 +0530

    added helper functions

--- main.go
+++ main.go
@@ -3,12 +3,70 @@ package main
 import(
 	"fmt"
 	"flag"
+	"encoding/json"
+	"os"
 )
 
+type Task struct{
+	Id int 				`json:"id"`
+	Name string 		`json:"name"`
+	Status string 		`json:"status"`
+
+}
+
 func main(){
+	
+
 	add := flag.String("add", "N/A", "accepts task(string) and adds it to the list")
-	rm := flag.Int("rm", '-1', "removes the tasks from the list. Takes in an index(int)")
-	done := flag.Int("done", '-1',"marks the task as done. Accepts an index(int)")
-	
-	
+	rm := flag.Int("rm", -1, "removes the tasks from the list. Takes in an index(int)")
+	done := flag.Int("done", -1,"marks the task as done. Accepts an index(int)")
+
+	_ = rm
+	_ = done
+		
+	data := getData()
+
+	fmt.Println(*add)
+
+	if(*add == "N/A"){
+		fmt.Println("added: ", *add)
+		newTask := Task{len(data), *add, "due"}
+		data := append(data, newTask)
+		updateData(data)
+	}
+	formatPrintData(data)
+}
+
+
+func formatPrintData(tasks []Task){
+	for _, value := range tasks{
+		fmt.Printf("%v  |   %s   			[%s]\n", value.Id, value.Name, value.Status)
+	}
+}
+
+func updateData(tasks []Task){
+		b, err:= json.Marshal(tasks)
+		if err != nil{
+			return
+		}
+
+		errr := os.WriteFile("task.json", b, 0666)
+		if errr != nil{
+			fmt.Println("Cant write to task.json:",errr)
+		}
+}
+func getData() []Task{
+
+	var tasks []Task
+	data, err := os.ReadFile("task.json")
+	if err != nil {
+		return tasks
+	}
+
+	err = json.Unmarshal(data, &tasks) // Check for Unmarshal errors
+	if err != nil {
+		return tasks
+	}
+
+	return tasks
 }
